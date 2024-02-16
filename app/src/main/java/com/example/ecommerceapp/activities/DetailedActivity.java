6c1e011f126fd1f5570db79375bda72363748e04
govindk1327 <govind1327k@gmail.com>
2024-02-16 19:03:51 +0530

    Jan 21 Update

--- app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
+++ app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -120,6 +121,15 @@ public class DetailedActivity extends AppCompatActivity {
             totalPrice = showAllModel.getPrice() * totalQuantity;
         }
 
+        //Buy Now
+        buyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DetailedActivity.this,AddressActivity.class));
+            }
+        });
+
+        //Add To Cart
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
